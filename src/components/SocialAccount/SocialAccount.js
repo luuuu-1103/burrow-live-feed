ef0242a2834c6d755593b30ce0c2c1556ef3ee20
Evgeny Kuzyakov <ek@nearprotocol.com>
2022-03-29 09:01:58 -0700

    Shorten long account ids

--- src/components/SocialAccount/SocialAccount.js
+++ src/components/SocialAccount/SocialAccount.js
@@ -2,15 +2,17 @@ import React from "react";
 import DefaultAvatar from "../../images/sasha_anon.png";
 import { useSocialAccount } from "../../data/socialAccount";
 import "./SocialAccount.scss";
+import { accountTrim } from "../../data/utils";
 
 export default function SocialAccount(props) {
   const accountId = props.accountId;
   const clickable = props.clickable;
   const socialAccount = useSocialAccount(accountId);
   const avatarUrl = socialAccount?.avatar?.url || DefaultAvatar;
+  const shortenAccountId = accountTrim(accountId);
   const displayName = socialAccount?.displayName
-    ? `${socialAccount.displayName} (${accountId})`
-    : accountId;
+    ? `${socialAccount.displayName} (${shortenAccountId})`
+    : shortenAccountId;
   const accountUrl = `https://explorer.near.org/accounts/${accountId}`;
   const inner = (
     <div className="social-account">
